ecc176223a4027e4a431cee5b31fbfdb7a5511ce
Usamaumer <usamaumer41407@gmail.com>
2021-03-20 20:45:34 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+        function functionalert(){
+            alert('Here is alert message');
+        }
+        
+        let mainNav = document.getElementById("js-menu");
+      let navBarToggle = document.getElementById("js-navbar-toggle");
+
+      navBarToggle.addEventListener("click", function() {
+                mainNav.classList.toggle("active");
+            });
+
+
+
+function myFunction() {
+          var x = document.getElementById("myPassword").value;
+          document.getElementById("Output").innerHTML = x;
+        }
+    
+
+function functionalert(){
+    alert('Here is the alert function calling.');
+}
